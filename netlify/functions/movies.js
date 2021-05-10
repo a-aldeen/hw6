1ca7427cfee5b945ed6fd5475ac52183bf8d7fc2
Alaa <alaa.aldeen@kellogg.northwestern.edu>
2021-05-09 19:29:35 -0500

    changed

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -7,7 +7,7 @@ let fs = require('fs')
 // defines a lambda function
 exports.handler = async function(event) {
   // write the event object to the back-end console
-  console.log(event)
+  // console.log(event)
 
   // read movies CSV file from disk
   let moviesFile = fs.readFileSync(`./movies.csv`)
@@ -16,16 +16,22 @@ exports.handler = async function(event) {
   let moviesFromCsv = await csv(moviesFile)
 
   // write the movies to the back-end console, check it out
-  console.log(moviesFromCsv)
+  // console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
+
+  //create a new object and hold the movies data
+  let moviesToReturn = {}
+
+  //start with an empty array
+  moviesToReturn.movies = []
   
-  if (year == undefined || genre == undefined) {
+  if (year == undefined || genre == undefined || year == ``|| genre==``) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Nope! Please define the genre and year!` // a string of data
     }
   }
   else {
@@ -35,13 +41,38 @@ exports.handler = async function(event) {
     }
 
     for (let i=0; i < moviesFromCsv.length; i++) {
+      
+      //store each movie in memory
+      let movie = moviesFromCsv[i]
+
+      // set condition if movie fits the required parameters requested by the user
+      if (movie.startYear == year && movie.genres.includes(genre)){
+
+        //create an object that includes the title, the year and teh movie's genres
+        let movieDetails = {
+          title: movie.primaryTitle,
+          year: movie.startYear,
+          genres: movie.genres
+        }
+        
+        //Add a condition to only push when there's no genre and when there's no runtime minutes
+        if (movie.genres != '\\N' && movie.runtimeMinutes != `\\N`) {
+
+          //add the movie to the list to push
+          moviesToReturn.movies.push(movieDetails)
+      }
+        console.log(movieDetails)
+      }
+
+    //add the number of movies to the returned object count
+    moviesToReturn.numResults = moviesToReturn.movies.length
 
     }
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(moviesToReturn) // a string of data
     }
   }
 }
